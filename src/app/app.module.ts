bfb9036c07263a58d5c1e6a831105429f1b45714
DavidKahumbi <david.kahumbi@student.moringaschool,com>
2022-04-19 03:18:25 +0300

    Add Http client to root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,6 +28,7 @@ import { UserDetailsComponent } from './user-details/user-details.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule
     
   ],
   providers: [],
